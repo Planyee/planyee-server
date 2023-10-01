e9b9b863eca11e10f0320075cb7e180e8e22f2b1
1Bronze <khdkkhdd@gmail.com>
2023-10-01 22:00:16 +0900

    [init] first commit

---
+++ src/main/java/com/gdsc2023/planyee/PlanyeeApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc2023.planyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanyeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanyeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdsc2023/planyee/PlanyeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc2023.planyee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanyeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
