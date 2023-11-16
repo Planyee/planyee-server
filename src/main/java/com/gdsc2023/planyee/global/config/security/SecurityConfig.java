90daa247397ce5f97c06f34cd419f7e450a25ed1
1Bronze <khdkkhdd@gmail.com>
2023-11-16 15:28:17 +0900

    fix: oauth 오류수정

--- src/main/java/com/gdsc2023/planyee/global/config/security/SecurityConfig.java
+++ src/main/java/com/gdsc2023/planyee/global/config/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 도메인 허용
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 도메인 허용
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(
                 Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin",
