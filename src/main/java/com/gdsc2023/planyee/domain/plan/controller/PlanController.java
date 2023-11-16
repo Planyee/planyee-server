c52f135a21a626a2862625403ba39fb50cbf0ce9
1Bronze <khdkkhdd@gmail.com>
2023-11-16 10:53:28 +0900

    fix: oauth2 오류 수정

--- src/main/java/com/gdsc2023/planyee/domain/plan/controller/PlanController.java
+++ src/main/java/com/gdsc2023/planyee/domain/plan/controller/PlanController.java
@@ -1,5 +1,6 @@
 package com.gdsc2023.planyee.domain.plan.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,6 @@ import com.gdsc2023.planyee.domain.plan.dto.PlanDetail;
 import com.gdsc2023.planyee.domain.plan.dto.PlanSummary;
 import com.gdsc2023.planyee.domain.plan.dto.PlanCreateRequest;
 import com.gdsc2023.planyee.domain.plan.service.PlanService;
-import com.gdsc2023.planyee.global.config.oauth.LoginUser;
 import com.gdsc2023.planyee.global.config.oauth.SessionUser;
 import lombok.RequiredArgsConstructor;
 
@@ -20,10 +20,9 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class PlanController {
     private final PlanService planService;
-    private final HttpSession httpSession;
 
     @GetMapping("/main")
-    public List<PlanSummary> getPlanSummarys() {
+    public List<PlanSummary> getPlanSummarys(HttpSession httpSession) {
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
         return planService.getPlanSummarys(user.getId());
     }
@@ -34,7 +33,7 @@ public class PlanController {
     }
 
     @PostMapping("/main")
-    public Long createPlan(@RequestBody PlanCreateRequest request) {
+    public Long createPlan(HttpSession httpSession, @RequestBody PlanCreateRequest request) {
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
         System.out.println(request.getPlanName());
         System.out.println(request.getSourceLatitude());
